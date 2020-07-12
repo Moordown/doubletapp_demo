63a7f5d7a3b4bf411d5f263c7da9af9641f228d6
Sergeev Victor <pappy.mot@gmail.com>
2020-07-12 15:05:23 +0500

    add docker

--- doublet_app_demo/settings.py
+++ doublet_app_demo/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xq#e4r(_=u#g_uf&d^5%&-9hbkb8m*o*shvdpdyqsdyyiq!pa2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
