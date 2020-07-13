71d6ece316a8f23ae4a5c57a65aca1b19ae5d1eb
Sergeev Victor <pappy.mot@gmail.com>
2020-07-13 08:48:59 +0500

    add gitignore

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Sergeev Viktor\PycharmProjects\doublet_app_demo\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
